35b45bb1b05dddc21b4d1a6b023abdfc44155c9f
Schetty <nps259@nyu.edu>
2016-09-13 17:53:53 -0700

    storyboard autolayout exercise

---
+++ StoryBoardAutolayout/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  StoryBoardAutolayout
+//
+//  Created by naomi schettini on 2016-09-13.
+//  Copyright © 2016 naomi schettini. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
